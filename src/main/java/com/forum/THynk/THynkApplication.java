df91433a839bd0219fc06ff2e995756ce7b35d56
phunguyendinh <phu0981838248@gmail.com>
2026-03-29 00:09:09 +0700

    first commit

---
+++ src/main/java/com/forum/THynk/THynkApplication.java
@@ -0,0 +1,13 @@
+package com.forum.THynk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class THynkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(THynkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/forum/THynk/THynkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forum.THynk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class THynkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
