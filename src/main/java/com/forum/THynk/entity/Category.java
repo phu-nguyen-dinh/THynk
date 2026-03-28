32eae6a95e9bf80f1b1093daaa418f0d2630a614
phunguyendinh <phu0981838248@gmail.com>
2026-03-29 00:16:22 +0700

    feat(entity): adding Category entity

---
+++ src/main/java/com/forum/THynk/entity/Category.java
@@ -0,0 +1,19 @@
+package com.forum.THynk.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+}
