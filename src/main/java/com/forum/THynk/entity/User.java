d5146f80c92e5f5c72aba27da1df68537f8f5e1a
phunguyendinh <phu0981838248@gmail.com>
2026-03-29 00:14:23 +0700

    feat(entity): adding User entity

---
+++ src/main/java/com/forum/THynk/entity/User.java
@@ -0,0 +1,37 @@
+package com.forum.THynk.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "full_name")
+    private String fullName;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "reputation")
+    private BigDecimal reputation;
+}
