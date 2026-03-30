0700190517baf1927303a172ff89495bfa034d1d
phunguyendinh <phu0981838248@gmail.com>
2026-03-30 22:26:28 +0700

    feat(service): adding post and thread service interface

--- src/main/java/com/forum/THynk/service/PostService.java
+++ src/main/java/com/forum/THynk/service/PostService.java
@@ -1,5 +1,20 @@
 package com.forum.THynk.service;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.forum.THynk.dto.req.PostRequestDTO;
+import com.forum.THynk.dto.res.PostResponseDTO;
+
+import jakarta.persistence.PostRemove;
+
 public interface PostService {
-    
+    PostRemove createPost(PostRequestDTO request, Long currentUserId);
+    Page<PostResponseDTO> getPostsByThread(Long threadId, Pageable pageable);
+    List<PostResponseDTO> getRepliesByPost(Long postId);
+    PostResponseDTO getPostById(Long postId);
+    PostResponseDTO updatePost(Long postId, String newContent, Long currentUserId);
+    void deletePost(Long postId, Long currentUserId);
 }
--- src/main/java/com/forum/THynk/service/ThreadService.java
+++ src/main/java/com/forum/THynk/service/ThreadService.java
@@ -1,5 +1,18 @@
 package com.forum.THynk.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.forum.THynk.dto.req.ThreadRequestDTO;
+import com.forum.THynk.dto.res.ThreadResponseDTO;
+
 public interface ThreadService {
-    
+    ThreadResponseDTO CreateThread(ThreadRequestDTO request);
+    Page<ThreadResponseDTO> GetAllThreads();
+    ThreadResponseDTO GetThreadById(Long id);
+    Page<ThreadResponseDTO> GetThreadsByCategoryId(Long categoryId, Pageable pageable);
+    Page<ThreadResponseDTO> searchThreads(String keyword, Pageable pageable);
+    void incrementViewCount(Long id);
+    void LockThread(Long threadId, Long userId);
+    void pinThread(Long threadId, Long userId);
 }
